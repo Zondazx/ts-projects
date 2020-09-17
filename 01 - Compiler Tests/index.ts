85faff32308a9b3fe74ed0c921eb5b0cc527c00c
Zondazx <mzxgd16@gmail.com>
2020-09-17 14:15:42 -0400

    first commit

---
+++ 01 - Compiler Tests/index.ts
@@ -0,0 +1,12 @@
+
+/**
+ * Run 'tsc' to compile.
+ * 
+ * Run 'tsc -p' for the TypeScript compiler to watch
+ * for changes in the files and automatically trigger the transpilation.
+ */
+function greet(person: string) {
+    console.log(`Good day ${person}`);
+}
+
+greet("Manuel");
---
+++ README.md
@@ -0,0 +1,1 @@
+# No detailed info yet.
