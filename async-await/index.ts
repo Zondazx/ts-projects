83911c0f1acbcd861b9dbe995969865f9d13b92a
Zondazx <mzxgd16@gmail.com>
2020-11-16 17:35:04 -0400

    fix: duplicated project

--- async-await/index.ts
+++ async-await/index.ts
@@ -1,8 +1,27 @@
 /**
  * Run 'tsc' to compile.
- * 
+ *
  * Run 'tsc -p' for the TypeScript compiler to watch
  * for changes in the files and automatically trigger the transpilation.
  */
-const hey: string = "hey";
-console.log(hey);
+const getRandomInteger = (): string => {
+  return (Math.random() * 10).toFixed(0);
+};
+
+const findEven = new Promise<number>((resolve, reject) => {
+  console.log(getRandomInteger());
+  setTimeout((): void => {
+    const value = parseInt(getRandomInteger());
+
+    if (value % 2 === 0) {
+      resolve(value);
+    } else {
+      reject('Odd number found!');
+    }
+  }, 1000);
+});
+
+findEven
+  .then(value => console.log('Resolved:', value + 1))
+  .catch(error => console.log('Rejected:', error))
+  .finally(() => console.log('Finished'));
